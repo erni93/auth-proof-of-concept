12c845dc85f9d7f1c0eec85f3781640b8e2395d1
Ernesto del valle <erni.delvalle@outlook.com>
2022-01-23 23:17:54 +0100

    add user repository

---
+++ user/repository/user-repository.go
@@ -0,0 +1,66 @@
+package user
+
+import (
+	"authGo/user"
+	"errors"
+)
+
+var ErrUserNotFound = errors.New("user repository: user not found")
+
+type UserRepository struct {
+	users []*user.User
+}
+
+func New() *UserRepository {
+	return &UserRepository{users: make([]*user.User, 0)}
+}
+
+func (r *UserRepository) Add(user *user.User) {
+	r.users = append(r.users, user)
+}
+
+func (r *UserRepository) GetById(id string) (*user.User, error) {
+	i, err := r.getIndexById(id)
+	if err != nil {
+		return nil, err
+	}
+	return r.users[i], ErrUserNotFound
+}
+
+func (r *UserRepository) GetByName(name string) (*user.User, error) {
+	i, err := r.getIndexByName(name)
+	if err != nil {
+		return nil, err
+	}
+	return r.users[i], ErrUserNotFound
+}
+
+func (r *UserRepository) Delete(id string) error {
+	i, err := r.getIndexById(id)
+	if err != nil {
+		return err
+	}
+	lastIndex := len(r.users) - 1
+	r.users[i] = r.users[lastIndex]
+	r.users[lastIndex] = nil
+	r.users = r.users[:lastIndex]
+	return nil
+}
+
+func (r *UserRepository) getIndexById(id string) (int, error) {
+	for i, user := range r.users {
+		if user.Id == id {
+			return i, nil
+		}
+	}
+	return -1, ErrUserNotFound
+}
+
+func (r *UserRepository) getIndexByName(name string) (int, error) {
+	for i, user := range r.users {
+		if user.Name == name {
+			return i, nil
+		}
+	}
+	return -1, ErrUserNotFound
+}
