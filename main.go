c1f64d82fb06be4e13bb8a2142f2fd9d1f8caa41
Ernesto del valle <erni.delvalle@outlook.com>
2022-07-08 00:20:02 +0200

    run server demo with login router

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"authGo/router"
+	"authGo/session"
+	"authGo/token"
+	"authGo/user"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello")
+	port := ":8181"
+	loginRouter := &router.LoginRouter{
+		UserService:           user.NewUserService(),
+		SessionHandler:        session.NewSessionHandler(),
+		AccessTokenGenerator:  &token.TokenGenerator[token.AccessTokenPayload]{Password: []byte("accessKey"), Duration: time.Minute * 2},
+		RefreshTokenGenerator: &token.TokenGenerator[token.RefreshTokenPayload]{Password: []byte("refreshKey"), Duration: time.Hour * 24 * 365},
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/auth/login", loginRouter.Handler).Methods("POST")
+	http.Handle("/", router)
+	log.Printf("Application listening on port %s", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
